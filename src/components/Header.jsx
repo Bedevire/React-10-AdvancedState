bf6da8d1e75475d42488f25410b4214f3f3bd232
Adam Pavlik <adam.pavlik@exxeta.com>
2024-02-19 14:58:11 +0100

    Use Context instead of props drilling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
+import { CartContext } from '../store/shopping-cart-context'
 
 import CartModal from './CartModal.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header({ }) {
   const modal = useRef();
-
-  const cartQuantity = cart.items.length;
+  const cartCtx = useContext(CartContext);
+  const cartQuantity = cartCtx.items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -26,8 +27,8 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+        cartItems={cartCtx.items}
+        //onUpdateCartItemQuantity={onUpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
       />
